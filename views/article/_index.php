76168180b9bd43fb12f214c90185af2cf7279c6c
galusmarcin87 <galusmarcin87@gmail.com>
2024-01-02 15:22:23 +0100

    hp

--- views/article/_index.php
+++ views/article/_index.php
@@ -6,28 +6,28 @@ use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\mgcms\db\Article */
-$displayImageHook = isset($displayImageHook) ? $displayImageHook : false;
+$model->language = Yii::$app->language;
 ?>
 
-<?if($index == 2 && $displayImageHook):?>
-<div class="news decoration">
-    <img src="/images/home-news.png" alt="" class="img-fluid">
-</div>
-</div>
-<div class="col-lg-4">
-
-<?endif;?>
-
-<a class="card card-light mb-3" href="<?= $model->linkUrl ?>">
+<a class="card card-dark mb-3" href="<?= $model->linkUrl ?>">
+    <div class="card-main-image img-corner-left-top">
+        <? if ($model->file && $model->file->isImage()): ?>
+            <img src="<?= $model->file->getImageSrc(450, 271); ?>" class="card-img-top" alt="<?= $model ?>"/>
+        <? endif; ?>
+        <img src="/images/tokens-img.jpg" class="card-img-top" alt="Token">
+        <div class="card-main-image-overlay">
+            <span class="btn">Poznaj szczegóły</span>
+        </div>
+    </div>
     <div class="card-body">
-
-        <h5 class="card-title card-title--news">
-            <?= $model->title ?>
-        </h5>
         <div class="card-date">
             <?= $model->created_on ?>
         </div>
+        <h5 class="card-title--news">
+            <?= $model->title ?>
+        </h5>
         <?= $model->excerpt ?>
-        <span class="card-readmore"><?= Yii::t('db', 'Find out more') ?></span>
+        <span class="card-readmore"><?= Yii::t('db', 'Read more') ?></span>
     </div>
 </a>
+
--- views/site/index/news.php
+++ views/site/index/news.php
@@ -1,5 +1,6 @@
 <?
 /* @var $this yii\web\View */
+
 // sekcja aktualnosci na home
 
 use app\components\mgcms\MgHelpers;
@@ -8,12 +9,12 @@ use yii\web\View;
 use app\models\mgcms\db\Article;
 use yii\widgets\ListView;
 
-$query = Article::find()->where(['status' => Article::STATUS_ACTIVE,'type'=>Article::TYPE_NEWS])->orderBy(['id' => SORT_DESC]);
+$query = Article::find()->where(['status' => Article::STATUS_ACTIVE, 'type' => Article::TYPE_NEWS])->orderBy(['id' => SORT_DESC]);
 
 $dataProvider = new ActiveDataProvider([
     'query' => $query,
     'pagination' => [
-        'pageSize' => 9,
+        'pageSize' => 3,
     ],
     'sort' => [
         'attributes' => [
@@ -25,38 +26,32 @@ $dataProvider = new ActiveDataProvider([
 ?>
 
 <div class="news-section section-dark-right py-5">
-	<div class="section-content">
-		<div class="section-title text-center"><?= Yii::t('db', 'News') ?></div>
-		<div class="section-background">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-4">
-						<a class="card card-dark mb-3" href="/posts-item.html">
-							<div class="card-main-image img-corner-left-top">
-								<img src="/images/tokens-img.jpg" class="card-img-top" alt="Token">
-								<div class="card-main-image-overlay">
-                                    <span class="btn">Poznaj szczegóły</span>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <div class="card-date">
-                                    21.05.2023
-                                </div>
-								<h5 class="card-title--news">
-                                            Energrtyka przyszłości EMC
-                                </h5>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                </p>
-                                <span class="card-readmore">Czytaj więcej</span>
-							</div>
-						</a>
-                    </div>
+    <div class="section-content">
+        <div class="section-title text-center"><?= Yii::t('db', 'News') ?></div>
+        <div class="section-background">
+            <div class="container">
+                <?=
+                ListView::widget([
+                    'dataProvider' => $dataProvider,
+                    'itemOptions' => [
+                        'class' => 'col-lg-4'
+                    ],
+                    'options' => [
+                        'class' => 'row',
+                    ],
+                    'layout' => '{items}',
+                    'itemView' => function ($model, $key, $index, $widget) {
+                        return $this->render('/article/_index', ['model' => $model, 'key' => $key, 'index' => $index, 'widget' => $widget, 'view' => $this]);
+                    },
+                ])
+
+                ?>
+
+                <div class="text-center my-5">
+                    <a href="<?= \yii\helpers\Url::to(['/article']) ?>"
+                       class="readmore btn btn-primary"><?= Yii::t('db', 'See all') ?></a>
+                </div>
             </div>
-				<div class="text-center my-5">
-					<a href="<?= \yii\helpers\Url::to(['/article']) ?>" class="readmore btn btn-primary"><?= Yii::t('db', 'See all') ?></a>
-				</div>
         </div>
     </div>
-	</div>
 </div>
